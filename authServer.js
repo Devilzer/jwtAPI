2eb23d2b1cf0b8e51ef065987d0f53a25dec1e07
Deepak Jena <jenadeepak724@gmail.com>
2020-09-27 02:30:16 +0530

    added jwt on authserver and data on default server

--- authServer.js
+++ authServer.js
@@ -5,15 +5,46 @@ require("dotenv").config();
 
 const port = 3000;
 app.use(express.json());
+
+let refreshTokens = [];
+//login using jwt
 app.post("/login", (req, res) => {
   const user = {
     name: req.body.name,
   };
-  const token = jwt.sign(user, process.env.AUTH_SECRET_KEY);
+  const accessToken = jwt.sign(user, process.env.AUTH_SECRET_KEY, {
+    expiresIn: "30s",
+  });
+  const refreshToken = jwt.sign(user, process.env.AUTH_REFRESH_KEY);
+  refreshTokens.push(refreshToken);
   return res.status(200).json({
-    authToken: token,
+    accessToken: accessToken,
+    refreshToken: refreshToken,
   });
 });
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) {
+    return res.status(401);
+  }
+  if (!refreshTokens.includes(refreshToken)) {
+    return res.status(403);
+  }
+  jwt.verify(refreshToken, process.env.AUTH_REFRESH_KEY, (err, user) => {
+    if (err) {
+      return res.status(403);
+    }
+    const accessToken = jwt.sign(
+      { nme: user.name },
+      process.env.AUTH_SECRET_KEY,
+      { expiresIn: "30s" }
+    );
+    return res.status(200).json({
+      accessToken: accessToken,
+    });
+  });
+});
+
 app.get("/", (req, res) => {
   return res.status(200).json({
     message: "auth server",
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -17,9 +18,27 @@ const posts = [
   },
 ];
 
-app.get("/posts", (req, res) => {
-  return res.status(200).json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  return res.status(200).json({
+    message: `wellcome ${req.user.name}`,
+    posts: posts,
+  });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) {
+    return res.status(401);
+  }
+  jwt.verify(token, process.env.AUTH_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json(err);
+    }
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(port, () => {
   console.log("we are live on " + port);
